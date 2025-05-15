78e05ac4dca32b0fe4b1945663f337efa23c0b01
Sergei Gritsun <79272724+spgritsun@users.noreply.github.com>
2025-05-16 02:43:51 +0300

    внес изменения в main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-import pages.homepage
+import streamlit as st
+
+st.set_page_config(
+    page_title="My App",
+    layout="wide"
+)
+
+# Явно перечислите страницы (опционально)
+st.sidebar.markdown("""
+- [Page 1](?page=page1)
+- [Page 2](?page=page2)
+""")
