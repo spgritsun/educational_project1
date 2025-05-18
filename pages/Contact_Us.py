3c9ae2f0f6fa24f947ad5e2602863c4e18c21cbd
Sergei Gritsun <79272724+spgritsun@users.noreply.github.com>
2025-05-18 23:34:22 +0300

    select box is added

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,15 +1,18 @@
 import streamlit as st
 from functions import send_email
+import pandas as pd
 
+topic_list = [row['topic'] for index, row in pd.read_csv('topics.csv').iterrows()]
 st.header("Contact Us")
 
 with st.form(key='email'):
     visitor_email = st.text_input('Your email address', placeholder='example@mail.com')
+    topic = st.selectbox('What topic do you want to discuss?', topic_list)
     visitor_row_massage = st.text_area('Tipe you massage') + '\n' + visitor_email
     send_button = st.form_submit_button('Submit')
     subject = f"New mail from {visitor_email}"
     receiver = 'gritsun.sp@gmail.com'
-    message = f"From: {visitor_email}\r\nTo: {receiver}\r\nSubject: {subject}\r\n{visitor_row_massage}"
+    message = f"From: {visitor_email}\r\nTo: {receiver}\r\nSubject: {subject}\r\n{topic}\r\n{visitor_row_massage}"
     if send_button:
         send_email(receiver, message)
         st.info('The message was sent successfully')
