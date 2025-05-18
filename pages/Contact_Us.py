394d5fcd8fbf65a6610bf1ec1a41be8a15cee918
Sergei Gritsun <79272724+spgritsun@users.noreply.github.com>
2025-05-19 00:34:22 +0300

    select box is added and adjust

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -8,11 +8,12 @@ st.header("Contact Us")
 with st.form(key='email'):
     visitor_email = st.text_input('Your email address', placeholder='example@mail.com')
     topic = st.selectbox('What topic do you want to discuss?', topic_list)
-    visitor_row_massage = st.text_area('Tipe you massage') + '\n' + visitor_email
+    visitor_row_message = st.text_area('Tipe you message') + '\n' + visitor_email
     send_button = st.form_submit_button('Submit')
     subject = f"New mail from {visitor_email}"
     receiver = 'gritsun.sp@gmail.com'
-    message = f"From: {visitor_email}\r\nTo: {receiver}\r\nSubject: {subject}\r\n{topic}\r\n{visitor_row_massage}"
     if send_button:
+        message = f"From: {visitor_email}\r\nTo: {receiver}\r\nSubject: {subject}\r\n\r\n" \
+                  f"Topic: {topic}\r\n{visitor_row_message} "
         send_email(receiver, message)
         st.info('The message was sent successfully')
